6c61e8567faaee3a8311474506f1fbf6007ba5a7
Daniel <dan@nopro.be>
2013-07-18 11:24:43 +0100

    added arguments to specify the site to trawl

--- Load Testing/get_urls.py
+++ Load Testing/get_urls.py
@@ -1,14 +1,32 @@
 #!/usr/bin/python
 # requires installation of beautifulsoup to run
 __author__ = 'datkinson'
+import sys, getopt
 import httplib2
 import urllib
 from BeautifulSoup import BeautifulSoup, SoupStrainer
 
 http = httplib2.Http()
-URL = 'http://hourd.co.uk'
-status, response = http.request(URL)
+URL = ''
 LINKS = {}
+
+USAGE = "Usage: %s -u url_of_site_to_trawl"
+try:
+    myopts, args = getopt.getopt(sys.argv[1:],"u:")
+except getopt.GetoptError as e:
+    print (str(e))
+    print(USAGE % sys.argv[0])
+    sys.exit(2)
+
+for o, a in myopts:
+    if o == '-u':
+        URL = a
+if not URL:
+    print("Error: No URL specified")
+    print(USAGE % sys.argv[0])
+    sys.exit(2)
+
+status, response = http.request(URL)
 
 def pageLinks(page_url):
 	# find all links on page
