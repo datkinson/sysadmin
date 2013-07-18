b4f2c07d4c1e86fcf52128fb1810a74bda46eec5
Daniel <dan@nopro.be>
2013-07-18 11:12:48 +0100

    changed url scraper into funtion and added incomplete check for existing urls

--- Load Testing/get_urls.py
+++ Load Testing/get_urls.py
@@ -10,19 +10,27 @@ URL = 'http://hourd.net'
 status, response = http.request(URL)
 LINKS = {}
 
-# find all links on page
-for link in BeautifulSoup(response, parseOnlyThese=SoupStrainer('a')):
-	# if the found link starts with the supplied url then add it to dict
-	if link['href'].startswith(URL):
-		if link['href'] in LINKS:
-			LINKS[link['href']] = LINKS[link['href']]+1
-		else:
-			LINKS[link['href']] = 1
+def pageLinks(page_url):
+	# find all links on page
+	global LINKS
+	for link in BeautifulSoup(response, parseOnlyThese=SoupStrainer('a')):
+		# if the found link starts with the supplied url then add it to dict
+		if link['href'].startswith(URL):
+			if link['href'] in LINKS:
+				LINKS[link['href']] = LINKS[link['href']]+1
+			else:
+				LINKS[link['href']] = 1
 # loop through the dict adding new links from the new links
 
 # --- need to add ---
-LIST_SIZE = 0
+pageLinks(URL)
+LIST_LENGTH = len(LINKS)
+TEMP_LIST_LENGTH = 0
+while LIST_LENGTH != TEMP_LIST_LENGTH:
+	for key, value in LINKS.items():
+		pageLinks(key)
+	TEMP_LIST_LENGTH = len(LINKS)
 
 # print the dict
 for key, value in LINKS.items():
-	        print "%s - %d" % (key, value)
+	print "%s - %d" % (key, value)
